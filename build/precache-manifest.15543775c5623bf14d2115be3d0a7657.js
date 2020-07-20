714547757a9f76cdcbaba856d68352b4498b45f4
Sushree P Mohanty <60944543+SushreeP@users.noreply.github.com>
2020-07-20 10:37:25 +0100

    Add build folder

---
+++ build/precache-manifest.15543775c5623bf14d2115be3d0a7657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c0089b79bc1bb9b1e87710d0e827ce",
+    "url": "/todo-list-app/index.html"
+  },
+  {
+    "revision": "dc30f71de495f212bc8c",
+    "url": "/todo-list-app/static/css/main.66866833.chunk.css"
+  },
+  {
+    "revision": "9990650e1a990d93594c",
+    "url": "/todo-list-app/static/js/2.7070af66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list-app/static/js/2.7070af66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc30f71de495f212bc8c",
+    "url": "/todo-list-app/static/js/main.ab6188cf.chunk.js"
+  },
+  {
+    "revision": "fc1ebefe855e12ff14a2",
+    "url": "/todo-list-app/static/js/runtime-main.d4521ec4.js"
+  }
+]);
